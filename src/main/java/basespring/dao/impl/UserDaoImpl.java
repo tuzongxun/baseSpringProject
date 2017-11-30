2faa1b52dd2ff65534a817746cbfb39ac54f964c
tuzongxun <1160569243@qq.com>
2017-11-30 17:20:45 +0800

    优化注解

--- src/main/java/basespring/dao/impl/UserDaoImpl.java
+++ src/main/java/basespring/dao/impl/UserDaoImpl.java
@@ -10,6 +10,7 @@ public class UserDaoImpl implements UserDao {
 
 	@Override
 	public UserModel findUserByName(String name) {
+		// 由于实际没有操作数据库，这里模拟从数据库中查询数据
 		System.out.println("查询参数：" + name);
 		UserModel user = new UserModel();
 		user.setAge(22);
